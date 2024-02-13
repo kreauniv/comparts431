e8e98524982e2298e479e7db09a919c365d09b0d
Srikumar Subramanian <srikumar.subramanian@krea.edu.in>
2024-02-13 09:19:07 +0530

    Fixed phasor and added documentation

--- source/synth.jl
+++ source/synth.jl
@@ -1,26 +1,69 @@
 
+"""
+    abstract type Signal end
+
+A `Signal` represents a process that can be asked for
+a value for every tick of a clock. We use it here to
+represent processes that produce audio and control
+signals to participate in a "signal flow graph".
+
+To construct signals and to wire them up in a graph,
+use the constructor functions provided rather than
+the structure constructors directly.
+
+The protocol for a signal is given by two functions
+with the following signatures --
+
+- `done(s :: S, t, dt) where {S <: Signal} :: Bool`
+- `value(s :: S, t, dt) where {S <: Signal} :: Float32`
+
+The renderer will call `done` to check whether a signal
+has completed and if not, will call `value` to retrieve the
+next value.
+
+Addition, subtraction and multiplication operations are
+available to combine signals and numbers.
+"""
 abstract type Signal end
 
+"Converts a dB value to a scaling factor"
 dBscale(v) = 10.0 ^ (v/10.0)
+
+"Converts a MIDI note number into a frequency using the equal tempered tuning."
 midi2hz(m) = 440.0 * (2 ^ ((m - 69.0)/12.0))
+
+"Converts a frequency in Hz to its MIDI note number in the equal tempered tuning."
 hz2midi(hz) = 69.0 + 12.0*log(hz/440.0)/log(2.0)
 
-mutable struct Alias{S <: Signal} <: Signal
+"""
+    mutable struct Aliasable{S <: Signal} <: Signal
+
+A signal, once constructed, can only be used by one "consumer".
+In some situations, we want a signal to be plugged into multiple
+consumers. For these occasions, make the signal aliasable by calling
+`aliasable` on it and use the alisable signal everywhere you need it
+instead. 
+
+**Note**: It only makes sense to make a single aliasable version of a signal.
+Repeated evaluation of a signal is avoided by `Aliasable` by storing the
+recently computed value for a given time. So it assumes that time progresses
+linearly.
+"""
+mutable struct Aliasable{S <: Signal} <: Signal
     sig :: S
     t :: Float64
     v :: Float32
 end
 
-alias(sig :: S) where {S <: Signal} = Alias{S}(sig, -1.0, 0.0f0)
-done(s :: Alias, t, dt) = done(s.sig, t, dt)
-function value(s :: Alias, t, dt)
+aliasable(sig :: S) where {S <: Signal} = Aliasable{S}(sig, -1.0, 0.0f0)
+done(s :: Aliasable, t, dt) = done(s.sig, t, dt)
+function value(s :: Aliasable, t, dt)
     if t > s.t
         s.t = t
         s.v = Float32(value(s.sig, t, dt))
     end
     return s.v
 end
-
 
 mutable struct Konst <: Signal
     k :: Float32
@@ -29,27 +72,41 @@ end
 value(s :: Konst, t, dt) = s.k
 done(s :: Konst, t, dt) = false
 
+"""
+    konst(v::Number)
+
+Makes a constant valued signal.
+"""
 function konst(v::Number)
     Konst(Float32(v))
 end
 
 mutable struct Phasor{F <: Signal} <: Signal
     freq :: F
-    phi :: Float32
+    phi :: Float64
 end
 
 function value(s :: Phasor{F}, t, dt) where {F <: Signal}
     val = s.phi
     s.phi += value(s.freq, t, dt) * dt
-    return Float32(val)
+    return Float32(mod(val,1.0f0))
 end
 
 function done(s :: Phasor{F}, t, dt) where {F <: Signal}
     done(s.freq, t, dt)
 end
 
-phasor(f :: Number, phi0 = 0.0f0) = Phasor(konst(f), phi0)
-phasor(f :: F, phi0 = 0.0f0) where {F <: Signal} = Phasor(f, phi0)
+"""
+    phasor(f :: Number, phi0 = 0.0) = Phasor(konst(f), phi0)
+    phasor(f :: F, phi0 = 0.0) where {F <: Signal} = Phasor(f, phi0)
+
+A "phasor" is a signal that goes from 0.0 to 1.0 linearly and then
+loops back to 0.0. This is useful in a number of contexts including
+wavetable synthesis where the phasor can be used to lookup the
+wavetable.
+"""
+phasor(f :: Number, phi0 = 0.0) = Phasor(konst(f), phi0)
+phasor(f :: F, phi0 = 0.0) where {F <: Signal} = Phasor(f, phi0)
 
 mutable struct SinOsc{Mod <: Signal, Ph <: Signal} <: Signal
     modulator :: Mod
@@ -66,7 +123,15 @@ function value(s :: SinOsc, t, dt)
     m * sin(Float32(2.0f0 * π * p))
 end
 
-sinosc(m :: Number, f :: Number) = SinOsc(konst(m), phasor(f))
+"""
+    sinosc(m :: Number, f :: Number) = SinOsc(konst(m), clock(f))
+    sinosc(m :: Number, p :: P) where {P <: Signal} = SinOsc(konst(m), p)
+    sinosc(m :: M, p :: P) where {M <: Signal, P <: Signal} = SinOsc(m, p)
+
+A "sinosc" is a sinusoidal oscillator that can be controlled using a
+phasor or a clock to determine a time varying frequency.
+"""
+sinosc(m :: Number, f :: Number) = SinOsc(konst(m), clock(f))
 sinosc(m :: Number, p :: P) where {P <: Signal} = SinOsc(konst(m), p)
 sinosc(m :: M, p :: P) where {M <: Signal, P <: Signal} = SinOsc(m, p)
 
@@ -130,6 +195,13 @@ function value(s :: Linterp, t, dt)
     end
 end
 
+"""
+    linterp(v1 :: Number, duration_secs :: Number, v2 :: Number)
+
+Makes a signal that produces `v1` for `t < 0.0` and `v2` for `t > duration_secs`.
+In between the two times, it produces a linearly varying value between
+`v1` and `v2`.
+"""
 linterp(v1 :: Number, duration_secs :: Number, v2 :: Number) = Linterp(Float32(v1), Float32(duration_secs), Float32(v2))
 
 mutable struct Expinterp <: Signal
@@ -141,7 +213,19 @@ mutable struct Expinterp <: Signal
     dlv :: Float32
 end
 
-expinterp(v1 :: Number, duration_secs :: Number, v2 :: Number) = Expinterp(Float32(v1), Float32(duration_secs), Float32(v2), log(Float32(v1)), log(Float32(v2)), log(Float32(v2/v1)))
+"""
+    expinterp(v1 :: Number, duration_secs :: Number, v2 :: Number)
+
+Similar to linterp, but does exponential interpolation from `v1` to `v2`
+over `duration_secs`. Note that both values must be `> 0.0` for this
+to be valid.
+"""
+function expinterp(v1 :: Number, duration_secs :: Number, v2 :: Number)
+    @assert v1 > 0.0
+    @assert v2 > 0.0
+    @assert durtation_secs > 0.0
+    Expinterp(Float32(v1), Float32(duration_secs), Float32(v2), log(Float32(v1)), log(Float32(v2)), log(Float32(v2/v1)))
+end
 
 done(s :: Expinterp, t, dt) = false
 function value(s :: Expinterp, t, dt)
@@ -153,16 +237,25 @@ end
 
 mutable struct ExpDecay{R <: Signal} <: Signal
     rate :: R
-    lval :: Float32
+    attack_secs :: Float64
+    logval :: Float32
 end
 
-expdecay(rate :: R) where {R <: Signal} = ExpDecay(rate, 0.0f0)
+"""
+    expdecay(rate :: R) where {R <: Signal}
+
+Produces a decaying exponential signal with a "half life" determined
+by 1/rate. It starts with 1.0. The signal includes a short attack
+at the start to prevent glitchy sounds.
+"""
+expdecay(rate :: R; attack_secs = 0.005) where {R <: Signal} = ExpDecay(rate, attack_secs, 0.0f0)
 
 done(s :: ExpDecay, t, dt) = s.lval < -15.0f0 || done(s.rate, t, dt)
 
 function value(s :: ExpDecay, t, dt)
-    v = exp(s.lval)
-    s.lval -= value(s.rate, t, dt) * dt
+    if t < s.attack_secs return t / s.attack_secs end
+    v = 2^(s.logval)
+    s.logval -= value(s.rate, t, dt) * dt
     return v
 end
 
@@ -183,6 +276,19 @@ mutable struct ADSR <: Signal
     t3 :: Float32
     t4 :: Float32
 end
+
+"""
+    adsr(
+        alevel :: Number, asecs :: Number, 
+        dsecs :: Number,
+        suslevel :: Number, sussecs :: Number,
+        relsecs :: Number
+    )
+
+Makes an "attack-decay-sustain-release" envelope.
+The decay and release phases are treated as exponential
+and the others stay linear.
+"""
 
 function adsr(
         alevel :: Number, asecs :: Number, 
@@ -230,6 +336,17 @@ mutable struct Sample <: Signal
     loop_i :: Int
 end
 
+"""
+    sample(samples :: Vector{Float32}; looping = false, loopto = 1.0) 
+
+Produces a sampled signal which samples from the given array as a source.
+It starts from the beginning and goes on until the end of the array,
+but can be asked to loop back to a specified point after that.
+
+- The `loopto` argument is specified relative (i.e. scaled) to the length
+  of the samples vector. So if you want to jump back to the middle, you give
+  `0.5` as the `loopto` value.
+"""
 function sample(samples :: Vector{Float32}; looping = false, loopto = 1.0) 
     Sample(samples, length(samples), 0, looping, 1 + floor(Int, loopto * length(samples)))
 end
@@ -272,6 +389,12 @@ function value(s :: Wavetable, t, dt)
     s.table[i] + frac * (s.table[j] - s.table[i])
 end
 
+"""
+    wavetable(table :: Vector{Float32}, amp :: Amp, phase :: Ph) where {Amp <: Signal, Ph <: Signal}
+
+A simple wavetable synth that samples the given table using the given phasor
+and scales the table by the given amplitude modulator.
+"""
 function wavetable(table :: Vector{Float32}, amp :: Amp, phase :: Ph) where {Amp <: Signal, Ph <: Signal}
     Wavetable(table, length(table), amp, phase)
 end
@@ -281,6 +404,7 @@ mutable struct Map{S <: Signal} <: Signal
     sig :: S
 end
 
+"Maps a function over the signal. The result is a signal."
 map(f, sig :: S) where {S <: Signal} = Map(f, sig)
 done(s :: Map, t, dt) = done(s.sig, t, dt)
 value(s :: Map, t, dt) = Float32(f(value(s.sig, t, dt)))
@@ -290,18 +414,51 @@ function linearmap(a1 :: Number, a2 :: Number, b1 :: Number, b2 :: Number, s :: 
     map(x -> b1 + rate * (x - a1), s)
 end
 
+"""
+    maketable(L :: Int, f)
+
+Utility function to construct a table for use with `wavetable`.
+`f` is passed values in the range [0.0,1.0] to construct the
+table of the given length `L`.
+"""
 maketable(L :: Int, f) = [f(Float32(i/L)) for i in 0:(L-1)]
 
-mutable struct Clock{T <: Signal} <: Signal
-    speed :: T
+"""
+    mutable struct Clock{S <: Signal} <: Signal
+
+Very similar to Phasor except that phasor does a modulus
+to the range [0.0,1.0] while Clock doesn't. So you can use
+Clock as a time keeper for a scheduler, for example, so
+scheduling can happen on a different timeline than real time.
+
+## Constructors
+
+- `clock(speed :: S, t_end; sampling_rate_Hz) where {S <: Signal}`
+- `clock_bpm(tempo_bpm, t_end; sampling_rate_Hz)`
+"""
+mutable struct Clock{S <: Signal} <: Signal
+    speed :: S
     t :: Float64
+    t_end :: Float64
     dt :: Float64
 end
 
-clock(tempo_bpm = 60.0, sampling_rate_Hz = 48000) = Clock(tempo_bpm/60.0, 0.0, 1.0/48000)
-copy(c::Clock) = Clock(c.speed, 0.0, c.dt)
+"""
+    clock(speed :: Number, t_end :: Number = Inf; sampling_rate_Hz = 48000)
+    clock_bpm(tempo_bpm=60.0, t_end :: Number = Inf; sampling_rate_Hz = 48000)
+    clock_bpm(tempo_bpm :: S, t_end :: Number = Inf; sampling_rate_Hz = 48000) where {S <: Signal}
+    clock(speed :: S, t_end :: Number = Inf; sampling_rate_Hz = 48000) where {S <: Signal}
 
-done(c :: Clock, t, dt) = done(c.speed, t, dt)
+Constructs different kinds of clocks. Clocks can be speed controlled.
+Clocks used for audio signals should be made using the `clock` constructor
+and those for scheduling purposes using `clock_bpm`.
+"""
+clock(speed :: Number, t_end :: Number = Inf; sampling_rate_Hz = 48000) = Clock(konst(speed), 0.0, t_end, 1.0/sampling_rate_Hz)
+clock_bpm(tempo_bpm=60.0, t_end :: Number = Inf; sampling_rate_Hz = 48000) = Clock(konst(tempo_bpm/60.0), 0.0, t_end, 1.0/sampling_rate_Hz)
+clock_bpm(tempo_bpm :: S, t_end :: Number = Inf; sampling_rate_Hz = 48000) where {S <: Signal} = Clock((1.0/60.0) * tempo_bpm, 0.0, t_end, 1.0/sampling_rate_Hz)
+clock(speed :: S, t_end :: Number = Inf; sampling_rate_Hz = 48000) where {S <: Signal} = Clock(speed, 0.0, t_end, 1.0/sampling_rate_Hz)
+
+done(c :: Clock, t, dt) = t > c.t_end || done(c.speed, t, dt)
 function value(c :: Clock, t, dt)
     v = c.t
     c.t += value(c.speed, t, dt) * dt
@@ -317,6 +474,12 @@ mutable struct Seq{Sch <: Signal} <: Signal
     active_i :: Int
 end
 
+"""
+    seq(clock :: Sch, triggers :: Vector{Tuple{Float64,Signal}}) where {Sch <: Signal}
+
+Sequences the given signals in a virtual timeline determined by the given
+clock. Use `clock_bpm` to make such a clock.
+"""
 function seq(clock :: Sch, triggers :: Vector{Tuple{Float64,Signal}}) where {Sch <: Signal}
     Seq(clock,
         triggers,
@@ -351,9 +514,16 @@ function value(s :: Seq, t, dt)
     return v
 end
 
-function render(s :: S, dur; maxamp=0.5) where {S <: Signal}
+"""
+    render(s :: S, dur_secs; maxamp=0.5) where {S <: Signal}
+
+Renders the given signal to a flat `Vector{Float32}`,
+over the given `dur_secs`. If the signal terminates before
+the duration is up, the result is truncated accordingly.
+"""
+function render(s :: S, dur_secs; maxamp=0.5) where {S <: Signal}
     dt = 1.0 / 48000.0
-    tspan = 0.0:dt:dur
+    tspan = 0.0:dt:dur_secs
     result = Vector{Float32}()
     for t in tspan
         if !done(s, t, dt)
@@ -365,6 +535,11 @@ function render(s :: S, dur; maxamp=0.5) where {S <: Signal}
     return rescale(maxamp, result)
 end
 
+"""
+    write(filename :: AbstractString, model::Sig, duration_secs :: AbstractFloat; sr=48000, maxamp=0.5) where {Sig <: Signal}
+
+Renders and writes raw `Float32` values to the given file.
+"""
 function write(filename :: AbstractString, model::Sig, duration_secs :: AbstractFloat; sr=48000, maxamp=0.5) where {Sig <: Signal}
     s = render(model, duration_secs; sr)
     open(filename, "w") do f
@@ -372,12 +547,24 @@ function write(filename :: AbstractString, model::Sig, duration_secs :: Abstract
     end
 end
 
+"""
+    read_rawaudio(filename :: AbstractString)
+
+Reads raw `Float32` values from the given file into a `Vector{Float32}`
+that can then be used with `sample` or `wavetable`.
+"""
 function read_rawaudio(filename :: AbstractString)
     o = Vector{Float32}(undef, filesize(filename) ÷ 4)
     read!(filename, o)
     return o
 end
 
+"""
+    rescale(maxamp :: Float32, samples :: Vector{Float32}) :: Vector{Float32}
+
+Rescales the samples so that the maximum extent fits within the given
+`maxamp`. The renderer automatically rescales to avoid clamping.
+"""
 function rescale(maxamp, samples)
     sadj = samples .- (sum(samples) / length(samples))
     amp = maximum(abs.(samples))
