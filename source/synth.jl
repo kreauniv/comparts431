be6e64f9088234c7d431b7598a9a2055afc6676a
Srikumar Subramanian <srikumar.subramanian@krea.edu.in>
2024-04-10 17:23:57 +0530

    Added play function

--- source/synth.jl
+++ source/synth.jl
@@ -1,4 +1,5 @@
 using Random
+import PortAudio as Au
 
 """
     abstract type Signal end
@@ -1398,4 +1399,74 @@ function raisedcos(x, overlap)
     end
 end
 
+
+"""
+    startaudio(callback)
+
+`callback` is a function that will be called like `callback(sample_rate,
+readqueue, writequeue)`. `sample_rate` indicates the output sampling rate.
+`readqueue` and `writequeue` are `Channel`s that accept either an audio buffer
+as a `Vector{Float32}` or `Val(:done)` to indicate completion of the audio
+process. The callback is expected to `take!` a buffer from the `readqueue`,
+fill it up and submit it to the `writequeue` using `put!`.
+
+When the audio generation is done, `callback` should `put!(writequeue,
+Val(:done))` and break out of its generation loop and return.
+
+It returns a function that can be called (without any arguments)
+to stop the audio processing.
+"""
+function startaudio(callback; blocksize=64)
+    function audiothread(stream, rq, wq)
+        #println("In audiothread $stream, $rq, $wq")
+        endmarker = Val(:done)
+        buf = take!(rq)
+        while buf != endmarker
+            Base.write(stream, buf)
+            put!(wq, buf)
+            buf = take!(rq)
+        end
+        close(stream)
+        close(wq)
+        close(rq)
+        #println("Audio thread done!")
+    end
+
+    stream = Au.PortAudioStream(0, 1)
+    rq = Channel{Union{Val{:done}, Vector{Float32}}}(2)
+    wq = Channel{Union{Val{:done}, Vector{Float32}}}(2)
+    #println("Writing empty buffers...")
+    put!(wq, zeros(Float32, blocksize))
+    put!(wq, zeros(Float32, blocksize))
+    
+    #println("Starting threads...")
+    Threads.@spawn audiothread(stream, rq, wq)
+    Threads.@spawn callback(stream.sample_rate, wq, rq)
+    return () -> put!(wq, Val(:done))
+end
+
+function play(signal, duration_secs; blocksize=64)
+    function callback(sample_rate, rq, wq)
+        #println("In callback $sample_rate, $rq, $wq")
+        dt = 1.0 / sample_rate
+        t = 0.0
+        endmarker = Val(:done)
+        while t < duration_secs && !done(signal, t, dt)
+            buf = take!(rq)
+            if buf != endmarker
+                fill!(buf, 0.0f0)
+                for i in eachindex(buf)
+                    buf[i] = value(signal, t, dt)
+                    t += dt
+                end
+                put!(wq, buf)
+            else
+                break
+            end
+        end
+        put!(wq, Val(:done))
+        #println("Audio generator done!")
+    end
+    startaudio(callback; blocksize)
+end
 
